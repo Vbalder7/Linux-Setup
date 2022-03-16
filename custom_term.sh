decaadd8f9cf09119887db299456ee9917098f7d
Vbalder7 <toritoab01@gmail.com>
2022-03-16 04:20:00 -0500

    config

--- custom_term.sh
+++ custom_term.sh
@@ -11,17 +11,3 @@ git config --global --replace-all color.diff.new "green bold"
 git config --global --replace-all color.status.added "green bold"
 git config --global --replace-all color.status.changed "yellow"
 git config --global --replace-all color.status.untracked "cyan"
-
-fancygit --color-scheme-batman
-
-fancygit --separator-default
-
-fancygit --disable-full-path
-
-fancygit --disable-time
-
-fancygit --enable-double-line
-
-fancygit --set-user-name vic
-
-fancygit --set-host-name deb
